e3f904a2f4ca500e55f1c1c86de9241e3034f2b9
vtessini <valentin-tessini@hotmail.com>
2026-04-03 19:25:23 -0300

    Agregar la estructura inicial del proyecto backend de transporte con configuración de Maven, archivos de propiedades y clases de aplicación.

---
+++ transport-backend/transport-backend/src/test/java/com/vtessini/transport_backend/TransportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtessini.transport_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
